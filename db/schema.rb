ce35f2181492ab2b96e7af9a88a27a9711f1a5cf
devmocks <bilalhussian785@gmail.com>
2025-03-12 14:39:14 +0500

    Devise Extended with additional info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_12_085637) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_12_093054) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -31,6 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_085637) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
